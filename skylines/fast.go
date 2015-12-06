31ffc1e69c8bf3e6a4a831054b825c852cbbab65
Nick Setzer <nsetzer@ccis.neu.edu>
2015-12-06 15:02:55 -0500

    remove unnecessary variable

--- skylines/fast.go
+++ skylines/fast.go
@@ -22,27 +22,27 @@ func SolveFast(buildings Buildings) (points []CriticalPoint) {
 	sort.Sort(buildings)
 	hq := make(CriticalPoints, 0)
 
-	idx := 0 // index of the last critical point found
 	for _, b := range buildings {
 
 		// process previous critical points to see if they have terminated
 		for len(hq) > 0 && hq.Peak().(*CriticalPoint).X < b.Left {
 			p := heap.Pop(&hq).(*CriticalPoint)
-			idx = p.X
-			removeInvalidPoints(&hq, idx)
+			removeInvalidPoints(&hq, p.X)
 			points = append(points, CriticalPoint{X: p.X, Y: hq.Max()})
 		}
 
-		// add the current building to the heap
 		current_height := hq.Max()
-		idx = b.Left
+		// add the current building to the heap
+		// store the height with the right edge so that we can
+		// remove the point from the heap at the correct time.
 		heap.Push(&hq, &CriticalPoint{X: b.Right, Y: b.Height})
-		removeInvalidPoints(&hq, idx) // if any
+		removeInvalidPoints(&hq, b.Left) // if any
 		if b.Height > current_height {
 			points = append(points, CriticalPoint{X: b.Left, Y: b.Height})
 		}
 	}
 
+	// process any remaining critical points
 	for len(hq) > 0 {
 		p := heap.Pop(&hq).(*CriticalPoint)
 		removeInvalidPoints(&hq, p.X)
